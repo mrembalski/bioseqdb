ab6e966f3fc9727697962f2f59d607542831603b
jgladkowski <20865290+jgladkowski@users.noreply.github.com>
2022-05-05 11:15:27 +0200

    Parameterize the script

--- fasta-to-sql/fasta-to-sql.py
+++ fasta-to-sql/fasta-to-sql.py
@@ -1,9 +1,13 @@
-f = open('QUERY.fasta', 'r')
+#!/usr/bin/python3
+
+import sys
+
+f = open(sys.argv[1], 'r')
 lines = f.readlines()
 n = int(len(lines))
 
-print('create table query (id bigserial, seq aa_seq);')
-print('INSERT INTO query (seq) VALUES')
+print('CREATE TABLE ' + sys.argv[2] + ' (id bigserial, seq aa_seq);')
+print('INSERT INTO ' + sys.argv[2] + ' (seq) VALUES')
 for i in range(n):
     if i % 2 == 1:
         print('(\'' + lines[i][:-1] + '\')' + (',' if i < n - 1 else ';'))
