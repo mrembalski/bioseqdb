87c8502e45b221b0bad7c2e94067109d72c900e9
jgladkowski <20865290+jgladkowski@users.noreply.github.com>
2022-05-05 11:09:13 +0200

    Add apostrophes

--- fasta-to-sql/fasta-to-sql.py
+++ fasta-to-sql/fasta-to-sql.py
@@ -6,4 +6,4 @@ print('create table query (id bigserial, seq aa_seq);')
 print('INSERT INTO query (seq) VALUES')
 for i in range(n):
     if i % 2 == 1:
-        print('(' + lines[i][:-1] + ')' + (',' if i < n - 1 else ';'))
+        print('(\'' + lines[i][:-1] + '\')' + (',' if i < n - 1 else ';'))
