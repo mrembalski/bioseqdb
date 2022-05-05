d6fd5718dd14adaa96a4b3d11b1d9dde5ed1131f
jgladkowski <20865290+jgladkowski@users.noreply.github.com>
2022-05-05 11:07:15 +0200

    Change bigint to bigserial

--- fasta-to-sql/fasta-to-sql.py
+++ fasta-to-sql/fasta-to-sql.py
@@ -1,11 +1,9 @@
-f = open('DB.fasta', 'r')
+f = open('QUERY.fasta', 'r')
 lines = f.readlines()
 n = int(len(lines))
 
-print('create table target (id bigint, seq aa_seq);')
-
+print('create table query (id bigserial, seq aa_seq);')
+print('INSERT INTO query (seq) VALUES')
 for i in range(n):
     if i % 2 == 1:
-        print('INSERT INTO target VALUES (' + str(int((i + 1) / 2)) + ', \'' + lines[i][:-1] + '\');')
-    # print(i, ' ', lines[i])
-# print(lines[0:10])
+        print('(' + lines[i][:-1] + ')' + (',' if i < n - 1 else ';'))
